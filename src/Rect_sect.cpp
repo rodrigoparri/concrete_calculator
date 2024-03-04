c7d725ed0cc737f7579ea82559528d87ac4f2df2
Rodrigo Parrilla Mesas <rodrigoparri@gmail.com>
2024-03-04 14:25:45 +0100

    created the rect_sect class

---
+++ src/Rect_sect.cpp
@@ -0,0 +1,9 @@
+#include "Rect_sect.h"
+
+Rect_sect::Rect_sect()
+	: m_b(0), m_h(0), m_f_ck(0) {}
+
+Rect_sect::Rect_sect(double b, double h, double f_ck) 
+	: m_b(b), m_h(h), m_f_ck(f_ck) {}
+
+
---
+++ src/Rect_sect.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Rect_sect
+{
+public:
+	double m_b;
+	double m_h;
+	double m_f_ck;
+public:
+	Rect_sect();
+	Rect_sect(double b, double h, double f_ck);
+
+};
